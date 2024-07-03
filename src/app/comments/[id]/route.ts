1031362e7f56da169d9363d1f06c5daea49761b7
Maxim Tyumenbaev <tyumenbaev.m@gmail.com>
2024-07-03 22:45:15 +0300

    Handling PATCH Request has been studied

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,10 +1,13 @@
+import { log } from "console"
 import { comments } from "../data"
+
+type contextType = {
+    params: { id: string }
+}
 
 export async function GET(
     req: Request,
-    { params }: {
-        params: { id: string }
-    }
+    {params}: contextType
 ) {
     const comment = comments.find(comment => comment.id === parseInt(params.id))
 
@@ -13,4 +16,17 @@ export async function GET(
     }
 
     return Response.json(comment)
+}
+
+export async function PATCH(req: Request, {params}: contextType) {
+    const body = await req.json()
+    const {text} = body
+
+    const index = comments.findIndex(comment => comment.id === parseInt(params.id))
+    log(body)
+    log(text)
+    log(comments[index])
+    comments[index].text = text
+
+    return Response.json(comments[index])
 }

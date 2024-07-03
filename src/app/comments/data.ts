4a6eb4fc6c0846c7b9c15b17dd1664b1c8e3a380
Maxim Tyumenbaev <tyumenbaev.m@gmail.com>
2024-07-03 22:16:42 +0300

    POST and GET has been studied

---
+++ src/app/comments/data.ts
@@ -0,0 +1,14 @@
+export const comments = [
+  {
+    id: 1,
+    text: "lorem ipsum dolor sit amet 1",
+  },
+  {
+    id: 2,
+    text: "lorem ipsum dolor sit amet 2",
+  },
+  {
+    id: 3,
+    text: "lorem ipsum dolor sit amet 3",
+  },
+];

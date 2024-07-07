7e50f5e7be274d69be7f43309859cb434fb432cc
Maxim Tyumenbaev <tyumenbaev.m@gmail.com>
2024-07-07 22:01:43 +0300

    Headers in Route Handlers has been studied

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,16 @@
-export async function GET() {
-    return new Response('api for profile')
+import { headers } from "next/headers";
+import { NextRequest } from "next/server"
+
+export async function GET(req: NextRequest) {
+    const requestHeaders = new Headers(req.headers)
+    const headerList = headers()
+    console.log(requestHeaders.get('Authorization'));
+    console.log(headerList.get('Authorization'));
+    
+    
+    return new Response('<h1>api for profile</h1>', {
+        headers: {
+            'Content-Type': 'text/html'
+        }
+    })
 }

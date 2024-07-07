cf350f1745f57aea360e6b9868c4c87d773e8609
Maxim Tyumenbaev <tyumenbaev.m@gmail.com>
2024-07-07 22:10:46 +0300

    Cookies in Route Handlers has been studied

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,16 +1,23 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextRequest } from "next/server"
 
 export async function GET(req: NextRequest) {
     const requestHeaders = new Headers(req.headers)
     const headerList = headers()
+    cookies().set('resultPerPage', '20')
+
+    const theme = req.cookies.get('theme')
+
     console.log(requestHeaders.get('Authorization'));
     console.log(headerList.get('Authorization'));
-    
+    console.log(theme);
+    console.log(cookies().getAll());
+    console.log(cookies().get('resultPerPage'));
     
     return new Response('<h1>api for profile</h1>', {
         headers: {
-            'Content-Type': 'text/html'
+            'Content-Type': 'text/html',
+            'Set-Cookie': "theme=dark",
         }
     })
 }
